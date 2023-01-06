358abbe454940b36d2bd53e18ada792e9e223ff9
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2023-01-07 00:14:06 +0700

    feat: add default layout to public routes

---
+++ src/components/layouts/default.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+import { Navbar } from '../navbar'
+
+export const DefaultLayout = () => {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
 import { RouteObject } from 'react-router-dom'
+import { DefaultLayout } from '~/components/layouts'
 import { LandingPage } from '~/features/misc'
 
 const publicRoutes: RouteObject[] = [
   {
-    path: '/',
-    element: <LandingPage />,
-  },
-  {
-    path: '/register',
-    element: <div>Register page</div>,
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />,
+      },
+    ],
   },
 ]
 
